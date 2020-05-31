98c700526354447658ea27afa5dd2acf8c260ae2
darah98 <darashalabi@gmail.com>
2020-05-31 20:08:16 +0300

    lab06

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# api-server
+# LAB - Class 06
+
+# Lab: HTTP and REST
+
+# Author: Darah
+
+# Links and Resources
+
+## [submission PR]()
+
+## [ci/cd]()
+
+## Setup
+
+### How to initialize/run your application
+- To run application: json-server --watch ./data/db.json
+
+### UML
+
+![]()
---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./data/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON server is alive');
+});
+
+router.render = (req, res) => {
+    res.jsonp({
+        count: res.locals.data.length,
+        results: [res.locals.data],
+    });
+    console.log(res.locals.data);
+};
